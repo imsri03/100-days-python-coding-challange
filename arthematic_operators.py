aecdc1c841f6b7c84e96c935dfaf911baff25e58
Srinadh Reddy Gade <srinadhreddy@Mac.attlocal.net>
2025-06-25 10:24:10 -0500

    Day 2:arthermatic calculators - + - * %

---
+++ arthematic_operators.py
@@ -0,0 +1,9 @@
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+
+print(f"addition: {a+b}")
+print(f"sub: {a-b}")
+print(f"mul: {a*b}")
+print(f"div: {a/b}")
+print(f"modulus: {a%b}")
+
